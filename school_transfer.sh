e8aab72f3c183a5267968a7402cdd84e20f264be
BSchoenth245 <brockschoenthaler@gmail.com>
2024-11-07 11:52:03 -0600

    Created function filetransfer and reorganized code accordingly

--- school_transfer.sh
+++ school_transfer.sh
@@ -15,109 +15,47 @@ strClass3=DevOps
 strClass3Dir=D:/Old_School_Folders/2024_Fall/$strClass3
 
 
+filetransfer () {
+    if ! test -d $2; then
+        mkdir $2
+    fi
+
+    #loops through files in class directory
+    for strFile in "$1"/*; do
+
+        strFileName=$(basename "$strFile")
+
+        #tests to see if any files/directories in the class directory
+        #already exist in class directory
+        if ! test -e "$2/$strFileName"; then
+            cp -r "$strFile" "$2"
+
+        #Gives option to overwrite existing version of file
+        else
+            echo Would you like to overwrite the \"$strFileName\" file/directory? y/n
+            read choice
+
+            if [ "$choice" = 'y' ]; then
+                cp -r "$strFile" $2
+            elif [ "$choice" = 'n' ]; then
+                echo skipping \"$strFileName\"
+            else
+                echo "Invalid Input"
+                exit 1
+            fi
+        fi
+
+    done
+}
+
+
 #Tests to see if the folder to hold the year's classes exists
 if ! test -d $strSchoolYearDir; then
     mkdir $strSchoolYearDir
 fi
 
-##### CLASS 1
+filetransfer ${strClass1} ${strClass1Dir}
 
-#checks to see if the directory for the class exists
-if ! test -d $strClass1Dir; then
-    mkdir $strClass1Dir
-fi
+filetransfer ${strClass2} ${strClass2Dir}
 
-#loops through files in class directory
-for strFile in "$strClass1"/*; do
-
-    strFileName=$(basename "$strFile")
-
-    #tests to see if any files/directories in the class directory
-    #already exist in class directory
-    if ! test -e "$strClass1Dir/$strFileName"; then
-        cp -r "$strFile" $strClass1Dir
-
-    #Gives option to overwrite existing version of file
-    else
-        echo Would you like to overwrite the \"$strFileName\" file/directory? y/n
-        read choice
-
-        if [ "$choice" = 'y' ]; then
-            cp -r "$strFile" $strClass1Dir
-        elif [ "$choice" = 'n' ]; then
-            echo skipping \"$strFileName\"
-        else
-            echo "Invalid Input"
-            exit 1
-        fi
-    fi
-
-done
-
-##### CLASS 2
-
-#checks to see if the directory for the class exists
-if ! test -d $strClass2Dir; then
-    mkdir $strClass2Dir
-fi
-
-#loops through files in class directory
-for strFile in "$strClass2"/*; do
-
-    strFileName=$(basename "$strFile")
-
-    #tests to see if any files/directories in the class directory
-    #already exist in class directory
-    if ! test -e "$strClass2Dir/$strFileName"; then
-        cp -r "$strFile" $strClass2Dir
-
-    #Gives option to overwrite existing version of file
-    else
-        echo Would you like to overwrite the \"$strFileName\" file/directory? y/n
-        read choice
-
-        if [ "$choice" = 'y' ]; then
-            cp -r "$strFile" $strClass2Dir
-        elif [ "$choice" = 'n' ]; then
-            echo skipping \"$strFileName\"
-        else
-            echo "Invalid Input"
-            exit 1
-        fi
-    fi
-
-done
-
-##### CLASS 3
-
-#checks to see if the directory for the class exists
-if ! test -d $strClass3Dir; then
-    mkdir $strClass3Dir
-fi
-
-#loops through files in class directory
-for strFile in "$strClass3"/*; do
-
-    strFileName=$(basename "$strFile")
-
-    #tests to see if any files/directories in the class directory
-    #already exist in class directory
-    if ! test -e "$strClass3Dir/$strFileName"; then
-        cp -r "$strFile" $strClass3Dir
-
-    #Gives option to overwrite existing version of file
-    else
-        echo Would you like to overwrite the \"$strFileName\" file/directory? y/n
-        read choice
-
-        if [ "$choice" = 'y' ]; then
-            cp -r "$strFile" $strClass3Dir
-        elif [ "$choice" = 'n' ]; then
-            echo skipping \"$strFileName\"
-        else
-            echo "Invalid Input"
-            exit 1
-        fi
-    fi
-
-done
+filetransfer ${strClass3} ${strClass3Dir}
